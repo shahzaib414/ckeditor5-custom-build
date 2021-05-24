9f555ee156b5a0ea1f55f431698bb838ee6d6e1a
Shahzaib Shahid <shahzaib.shahid414@gmail.com>
2021-05-24 17:39:38 +0500

    add Build

---
+++ build/translations/tt.js
@@ -0,0 +1,1 @@
+!function(t){const n=t.tt=t.tt||{};n.dictionary=Object.assign(n.dictionary||{},{Bold:"Калын",Code:"Код",Italic:"",Redo:"Кабатла",Strikethrough:"",Underline:"",Undo:""}),n.getPluralForm=function(t){return 0}}(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
